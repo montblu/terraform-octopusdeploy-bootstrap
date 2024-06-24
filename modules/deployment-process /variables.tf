636e9179b8e6a30494b44bb78c74e393b5fd8db0
Lucas Cañero <54242962+lucascanero@users.noreply.github.com>
2024-06-24 13:00:48 +0100

    remove default variables.tf

--- modules/deployment-process /variables.tf
+++ modules/deployment-process /variables.tf
@@ -19,13 +19,13 @@ variable "octopus_worker_tools_version" {
 variable "k8s_set_image_deployment_command" {
   description = "K8s set image command"
   type        = string
-  default     = "kubectl set image deployment/${each.key} ${each.key}=360379118495.dkr.ecr.us-east-1.amazonaws.com/ramp-${each.key}-stage-default:$(get_octopusvariable "Octopus.Release.Number") && kubectl rollout status deployment ${each.key}"
+  default     = ""
 }
 
 variable "k8s_set_image_cronjob_command" {
   description = "K8s set image command"
   type        = string
-  default     = "kubectl set image cronjob/${step.key} ${step.key}=360379118495.dkr.ecr.us-east-1.amazonaws.com/ramp-${each.key}-stage-default:$(get_octopusvariable "Octopus.Release.Number")"
+  default     = ""
 }
 
 variable "slack_webhook" {
