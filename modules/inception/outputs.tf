7a37b5f0087a19d61998b2beb87acf0c70accdbc
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2024-09-12 17:08:41 +0100

    modules: inception: octopus.tf: Remove not needed output

--- modules/inception/outputs.tf
+++ modules/inception/outputs.tf
@@ -4,13 +4,6 @@ output "octopus_space_id" {
   sensitive   = false
 }
 
-output "octopus_lifecycle" {
-  value       = try(octopusdeploy_lifecycle.main["${var.lifecycles[0].name}"].id, "")
-  description = "The Octopus lifecycle id"
-  sensitive   = false
-  depends_on  = [octopusdeploy_lifecycle.main]
-}
-
 output "octopus_project_group_name" {
   value       = try(data.octopusdeploy_project_groups.all.partial_name, "")
   description = "The Octopus project group."
