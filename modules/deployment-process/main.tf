1be445c6cd91518c7297d6fc7c73d13d714f6c51
Lucas Cañero <lucas.canero14@gmail.com>
2025-05-07 10:56:00 +0100

    revert ignore_changes

--- modules/deployment-process/main.tf
+++ modules/deployment-process/main.tf
@@ -289,7 +289,23 @@ EOT
       }
     }
   }
-
+lifecycle {
+    ignore_changes = [
+      step[0].run_kubectl_script_action,
+      step[0].properties,
+      step[1].run_kubectl_script_action,
+      step[1].properties,
+      step[2].run_kubectl_script_action,
+      step[2].properties,
+      step[3].run_kubectl_script_action,
+      step[3].properties,
+      step[0].run_script_action,
+      step[1].run_script_action,
+      step[2].run_script_action,
+      step[3].run_script_action,
+      step[4].run_script_action
+    ]
+  }
 }
 
 #####
