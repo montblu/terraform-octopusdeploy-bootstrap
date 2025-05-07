00b28b0efa19ebdad4b6bda6a77054878d3eccf6
Tiago Carrondo <tiago@montblu.eu>
2025-05-07 13:20:20 +0100

    provider version less strict

--- modules/deployment-process/versions.tf
+++ modules/deployment-process/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     octopusdeploy = {
       source  = "OctopusDeployLabs/octopusdeploy"
-      version = "0.42.0"
+      version = ">= 0.42.0"
     }
     curl2 = {
       source  = "DanielKoehler/curl2"
@@ -12,4 +12,3 @@ terraform {
     }
   }
 }
-
