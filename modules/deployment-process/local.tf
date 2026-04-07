647ec4474dece5c769b11ecba68e1d3b262c3182
Lucas Cañero <lucas.canero14@gmail.com>
2026-04-07 17:07:41 +0100

    fix cronjob default script

--- modules/deployment-process/local.tf
+++ modules/deployment-process/local.tf
@@ -37,19 +37,16 @@ kubectl set image "deployment/$DEPLOYMENT" $${CONTAINERS[@]}
 kubectl rollout status "deployment/$DEPLOYMENT"
 EOT
 
-  cronjobs_script_body = <<-EOT
-  #!/bin/bash
+set_cronjob_script_body = <<-EOT
+#!/bin/bash
 
 set -e
 
-DEPLOYMENT="$(get_octopusvariable "deployment_name")"
 RELEASENUMBER="$(get_octopusvariable "Octopus.Release.Number")"
 DOCKER_IMAGE="$(get_octopusvariable "ecr_url")/$DEPLOYMENT:$RELEASENUMBER"
 
-# Set Image
-kubectl patch cronjob ${each.value.cronjob} --type=json \
-  -p="[{'op':'replace','path':'/spec/jobTemplate/spec/template/spec/containers/0/image','value':'${DOCKER_IMAGE}'}]"
-}
+kubectl patch cronjob __CRONJOB__ --type=json \
+  -p="[{'op':'replace','path':'/spec/jobTemplate/spec/template/spec/containers/0/image','value':'$DOCKER_IMAGE'}]"
 EOT
 }
 
