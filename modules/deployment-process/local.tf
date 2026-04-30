5645c337a263a8c4487f39ead18e61b019145199
Lucas Cañero <lucas.canero14@gmail.com>
2026-04-30 15:19:38 +0100

    Add custom logic to set images for cronjob containers based on annotations

--- modules/deployment-process/local.tf
+++ modules/deployment-process/local.tf
@@ -44,9 +44,29 @@ set -e
 DEPLOYMENT="$(get_octopusvariable "deployment_name")"
 RELEASENUMBER="$(get_octopusvariable "Octopus.Release.Number")"
 DOCKER_IMAGE="$(get_octopusvariable "ecr_url")/$DEPLOYMENT:$RELEASENUMBER"
+# Get list of all the containers in the Cronjob including init containers
+IFS=" " read -r -a ALL_CONTAINERS <<< "$(kubectl get cronjob __CRONJOB__ -o jsonpath="{.spec.jobTemplate.spec.template.spec.containers[*].name} {.spec.jobTemplate.spec.template.spec.initContainers[*].name}")"
+# Container names selected from the value coming from annotation 'octopus.containers.set' in the deployment
+OCTOPUS_ANNOTATION=$(kubectl get cronjob __CRONJOB__ -o jsonpath="{.metadata.annotations.octopus\.containers\.set}")
+CONTAINERS=()
 
-kubectl patch cronjob __CRONJOB__ --type=json \
-  -p="[{'op':'replace','path':'/spec/jobTemplate/spec/template/spec/containers/0/image','value':'$DOCKER_IMAGE'}]"
+if [ -z "$OCTOPUS_ANNOTATION" ]
+then
+    # If the annotation is missing or empty we set the image for all containers
+    SELECTED_CONTAINERS=("$${ALL_CONTAINERS[@]}")
+else
+    # Otherwise we use the containers from the annotation
+    IFS="," read -a SELECTED_CONTAINERS <<< "$OCTOPUS_ANNOTATION"
+fi
+
+# Create an array with container names and image Ex:["container1=image" "container2=image"]
+for container in "$${SELECTED_CONTAINERS[@]}"
+do
+    CONTAINERS+=( "$container=$DOCKER_IMAGE" )
+done
+
+kubectl set image cronjob/__CRONJOB__ $${CONTAINERS[@]}
+
 EOT
 }
 
