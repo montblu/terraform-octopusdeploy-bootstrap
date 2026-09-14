e0627418a2f48031ee5a8720dfa97f8cbc7c72d7
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2026-09-14 21:25:38 +0100

    modules: k8s-connector: main.tf: Explain worker Ubuntu version doesn't matter

--- modules/k8s-connector/main.tf
+++ modules/k8s-connector/main.tf
@@ -3,7 +3,7 @@ resource "octopusdeploy_dynamic_worker_pool" "ubuntu" {
   count       = var.create_global_resources ? 1 : 0
   name        = "${var.octopus_project_group_name}-workers-Ubuntu"
   space_id    = var.octopus_space_id
-  worker_type = "UbuntuDefault" # Avoid pinning a specific Ubuntu version so it doesn't break when Octopus deprecates it
+  worker_type = "UbuntuDefault" # Ubuntu version doesn't matter: steps run inside our workertools container, not on the host directly
   is_default  = true
 }
 
